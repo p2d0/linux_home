4401221937018e807351f6e0317a3196bed908f0
Patriot720 <cerkin-3@yandex.ru>
2020-12-18 22:01:08 +0300

    warp vpn on click

--- .config/polybar/wg-status.sh
+++ .config/polybar/wg-status.sh
@@ -1,8 +1,35 @@
 #!/usr/bin/env bash
 
-status=$(wg show 2>&1);
-if [ -z  "$status" ]; then
-    echo " OFF"
-else
-    echo " ON"
-fi
+is_off(){
+    status=$(wg show 2>&1);
+    if [ -z  "$status" ]; then
+        return 0;
+    fi
+    return 1;
+}
+
+show_status(){
+    if is_off; then
+        echo " OFF"
+    else
+        echo " ON"
+    fi
+}
+
+wg-toggle(){
+    if is_off; then
+        wgcf update
+        wg-quick up ~/.config/polybar/wgcf-profile.conf
+    else
+        wg-quick down ~/.config/polybar/wgcf-profile.conf
+    fi
+}
+
+case "$1" in
+    --status)
+        show_status
+        ;;
+    *)
+        wg-toggle
+        ;;
+esac
