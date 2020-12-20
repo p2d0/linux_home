e4e403549388ec90258a425236d9c4bf2b7c575a
Patriot720 <cerkin-3@yandex.ru>
2020-12-20 15:54:25 +0300

    dmenu -> rofi

--- install_system.sh
+++ install_system.sh
@@ -12,7 +12,7 @@ fi
 home_dir=/home/$USER
 
 install_packages(){
-    yay -Sy dmenu spotify telegram-desktop ttf-font-awesome bumblebee-status feh redshift emacs  \
+    yay -Sy dmenu rofi spotify telegram-desktop ttf-font-awesome bumblebee-status feh redshift emacs  \
         i3lock guake gnome-terminal gnome-disk-utility picom polkit-gnome flameshot pasystray pulseaudio \
         polybar \
         pulseaudio-bluetooth albert breeze breeze-gtk panther-launcher-git fcitx fcitx-mozc fcitx-qt5 \
